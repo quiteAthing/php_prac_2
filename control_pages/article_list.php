aa4f36bd3d2154641912584a0431fd9dfab350a4
BATTLESURFACE <hailhailHaily@hotmail.com>
2017-05-09 09:48:25 +0800

    add article list

---
+++ control_pages/article_list.php
@@ -0,0 +1,18 @@
+<?php
+
+	require_once("db_connection.php");
+
+?>
+
+
+<?php
+	 $stmt="select a.title title,a.articleid articleid,b.alias alias,a.submitted submitted,a.lastupdate lastupdate from articles as a join members as b on a.author=b.memberid limit 10;";
+	 $rst=$conn->query($stmt)->fetchAll();
+	 $ret=array();
+	 foreach( $rst as $article){
+		 array_push($ret,array("articleid"=>$article["articleid"], "alias"=>$article["alias"],"title"=>$article["title"],"lastupdate"=>$article["lastupdate"],"submitted"=>$article["submitted"]));
+	 }
+	 echo json_encode($ret);
+	 
+
+?>
---
+++ visual_contents/articleList/scripts/func_articleLit.js
@@ -0,0 +1,24 @@
+
+
+function sendLogin(){
+	var field_acc=document.getElementById("acc");
+	var field_pw=document.getElementById("pw");
+	var acc=field_acc.value;
+	var pw=field_pw.value;
+	field_acc.value="";
+	field_pw.value="";
+	var xhr=new XMLHttpRequest();
+
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState==1){xhr.send();}
+		if(xhr.readyState==4 && xhr.status==200){
+		var data=JSON.parse(xhr.responseText);
+		document.getElementById("nx_logged_out").style.display="none";
+		document.getElementById("nx_logged_in").style.display="block";
+		document.getElementById("username").innerHTML=data.alias;
+		}
+	
+	}
+	var urlWithParam="http://localhost/php_prac_2_dep/control_pages/login.php?acc="+acc+"&pw="+pw;
+	xhr.open("GET",urlWithParam,true);
+}
